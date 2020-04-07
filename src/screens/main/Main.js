ebc38da859031a6ab1a396c40840e7273940f11c
Robson Maia <robsonmvieira@gmail.com>
2020-04-07 09:15:05 -0300

    feat: using useState feature to management state

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,17 +1,36 @@
-import React from 'react'
-
-import { Container, Label, InputSearch, FormContainer, BtnSubmit, BtnLabel } from './styles'
+import React, { useState } from 'react'
+import api from '../../services/api'
+import {
+  Container,
+  Label,
+  InputSearch,
+  FormContainer,
+  BtnSubmit,
+  BtnLabel
+} from './styles'
 
 export default function Main () {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState([])
+  const handlerSubmit = () => {
+    const url = `https://jobs.github.com/positions.json?description=${job}`
+    api.get(url).then(response => setJobs(response.data))
+      .catch(error => console.log(error))
+    setJob('')
+  }
   return (
     <Container>
       <Label>
         Search a new Position
       </Label>
       <FormContainer>
-        <InputSearch placeholder='search a new Job' />
-        <BtnSubmit><BtnLabel>Search</BtnLabel></BtnSubmit>
+        <InputSearch
+          value={job} onChangeText={v => setJob(v)}
+          placeholder='search a new Job'
+        />
+        <BtnSubmit onPress={handlerSubmit}><BtnLabel>Find</BtnLabel></BtnSubmit>
       </FormContainer>
+      <Label>{jobs.length}</Label>
     </Container>
   )
 }
