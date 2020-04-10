5be5ca2bcd9ba44963981c8e478286233a7f7b70
Robson Maia <robsonmvieira@gmail.com>
2020-04-10 19:35:21 -0300

    feat: show a indicator loading feedback ux

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import api from '../../services/api'
+import JobItem from '../../components/job/Job'
+import { ActivityIndicator, View, FlatList } from 'react-native'
 import {
   Container,
   Label,
@@ -12,13 +14,22 @@ import {
 export default function Main () {
   const [job, setJob] = useState('')
   const [jobs, setJobs] = useState([])
+  const [loading, setloading] = useState(false)
+
   const handlerSubmit = () => {
+    setloading(true)
+    setJobs([])
     const url = `https://jobs.github.com/positions.json?description=${job}`
-    api.get(url).then(response => setJobs(response.data))
+    api.get(url).then(response => {
+      setJobs(response.data)
+      setloading(false)
+    }
+    )
       .catch(error => console.log(error))
     setJob('')
   }
   return (
+
     <Container>
       <Label>
         Search a new Position
@@ -30,7 +41,17 @@ export default function Main () {
         />
         <BtnSubmit onPress={handlerSubmit}><BtnLabel>Find</BtnLabel></BtnSubmit>
       </FormContainer>
-      <Label>{jobs.length}</Label>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        {loading && <ActivityIndicator color='#fff' />}
+      </View>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={jobs}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <JobItem title={item.title} type={item.type} />}
+        />
+      </View>
+      <Label>Total: {jobs.length > 0 ? jobs.length : 0}</Label>
     </Container>
   )
 }
